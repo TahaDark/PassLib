95aba8063bba7c4c29daf3e51b826ba694709e61
Taha <tahamojtabavidev@gmail.com>
2022-10-28 11:17:05 +0330

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+let maindiv = document.querySelector("main")
+let passwords = []
+
+function getLib() {
+    
+    keys = Object.keys(localStorage)
+    i = keys.length;
+    
+    while ( i-- ) {
+        getplace = keys[i]
+        getpass = localStorage.getItem(keys[i])
+        passwords[i] = {
+            place: getplace,
+            pass: getpass,
+        }
+    }
+}
+
+function updateBars() {
+    getLib()
+    for (i in passwords) {
+        number = parseInt(i) + 1
+        place = passwords[i].place
+        pass = passwords[i].pass
+        console.log(number);
+        console.log(place)
+        console.log(pass)
+    
+        let passbardiv = document.createElement("div")
+        let numberspan = document.createElement("span")
+        numberspan.innerText = number
+        let namespan = document.createElement("span")
+        namespan.innerText = place
+        let passspan = document.createElement("span")
+        passspan.innerText = pass
+        passbardiv.appendChild(numberspan)
+        passbardiv.appendChild(namespan)
+        passbardiv.appendChild(passspan)
+    
+        maindiv.appendChild(passbardiv)
+    
+    }    
+}
+
+updateBars()
+
+function addPass(name,pass) {
+    localStorage.setItem(name,pass)
+    getLib()
+}
+
+
